e3bb1ab8050f51bcc8988270d6edc910abfbe65a
Ishamu Joseph <35691481+jishamu@users.noreply.github.com>
2018-10-25 11:36:48 +0200

    Create jamon.py

---
+++ jamon.py
@@ -0,0 +1,10 @@
+# M03
+# coding: utf­8
+comprar=str(input("Que has comprado:"))
+abduccion=str(input("Has hecho abducción:"))
+dia=str(input("Que dia es:"))
+estudiar=str("Estudias"))
+if (comprar=="jamon") or((abduccion=="si") and (dia=="j")) or (estudiar=="mucho")):
+    print ("aprobas m03")
+else:
+     ("no aprobas)
