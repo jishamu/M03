2557ec3ed489cdb07d271cd22246ecbaded4ae96
ism3769892 <ism3769892@g09.informatica.escoladeltreball.org>
2018-10-25 08:49:17 +0200

    first commit

---
+++ hum.py
@@ -0,0 +1,9 @@
+# coding: utf­8
+humedad = input("Indica la humedad:")
+if humedad >= 80:
+    print ("La humedad es", humedad, "Coge la paragua")
+    
+else:
+    print("La humedad es", humedad, "No coges la paragua")
+git remote add origin https://github.com/jishamu/M03.git
+
