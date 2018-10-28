a7cfa42bdbf997eabe5425f8419352079a6d0cd3
Ishamu Joseph <35691481+jishamu@users.noreply.github.com>
2018-10-28 19:23:43 +0100

    Update jubilat.py

--- jubilat.py
+++ jubilat.py
@@ -1,11 +1,10 @@
 # coding: utf­8
 sexe=input( "indique tu sexe:")
-jubilat=input("Estas jubilat?:)
-cabell=input("Color del teu cabell")
-if ((cabell=="roses") or (sexe=="d")and(jubilat="no")):
-   print("No apaguen"))
+jubilat=input("Estas jubilat?:")
+cabell=input("Color del teu cabell:")
+if ((cabell=="roses") or ((sexe=="d")and(jubilat=="no"))):
+    print("No apaguen")
 if ((jubilat=="no") and (sexe=="h")):
     print("1€")
 else:
-     print("015 EUR")
-   
+     print("0,15 EUR")
