9bcd273b2f2a97abf02d5da57522ea5e6db33fe7
Ishamu Joseph <35691481+jishamu@users.noreply.github.com>
2018-10-28 19:03:57 +0100

    Update apagar.py

--- apagar.py
+++ apagar.py
@@ -1,7 +1,7 @@
-anyo=( "indique to cuantos años tienes:")
+
+anyo=int(input( "indique to cuantos años tienes:"))
 if (anyo<5) or (anyo>=65):
    print("Gratis")
 else:
      print("2,15 EUR")
-
    
