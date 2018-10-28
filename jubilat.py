0aa253908b26b027cde5e7887a36ccf38cee9dbc
Ishamu Joseph <35691481+jishamu@users.noreply.github.com>
2018-10-28 19:17:03 +0100

    Create jubilat.py

---
+++ jubilat.py
@@ -0,0 +1,11 @@
+# coding: utf­8
+sexe=input( "indique tu sexe:")
+jubilat=input("Estas jubilat?:)
+cabell=input("Color del teu cabell")
+if ((cabell=="roses") or (sexe=="d")and(jubilat="no")):
+   print("No apaguen"))
+if ((jubilat=="no") and (sexe=="h")):
+    print("1€")
+else:
+     print("015 EUR")
+   
