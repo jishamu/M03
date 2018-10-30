d04c15a8e753410304208622866196fb8e90b669
Ishamu Joseph <35691481+jishamu@users.noreply.github.com>
2018-10-30 12:40:06 +0100

    Create edad2.py

---
+++ edad2.py
@@ -0,0 +1,6 @@
+edad=int(input("Indica tu edad:"))
+if((edad>=18)and(edad<=23)):
+    print("Con", edad, "puedes entrar en la sesión de jóvenes")
+
+else:
+    print("Con", edad, "no puedes entrar en la sesión de jóvenes")
