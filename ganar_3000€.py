6b5cf2cc56b24dd91066f344c5204dfb646dd04a
Ishamu Joseph <35691481+jishamu@users.noreply.github.com>
2018-11-05 22:56:38 +0100

    Update ganar_3000€.py

--- ganar_3000€.py
+++ ganar_3000€.py
@@ -11,10 +11,10 @@ if((casar=="si")and(edad>80)and(corazon=="si")and(casado=="si")):
   print("Puedes ganar 3000€ al mes")
 	
 estudiar=input("estudias:")
-M01=int(input("Cuantos has sacado:"))
-M02=int(input("Cuantos has sacado:"))
-M03=int(input("Cuantos has sacado:"))
-M05=int(input("Cuantos has sacado:"))
+M01=int(input("Cuantos has sacado M01:"))
+M02=int(input("Cuantos has sacado M02:"))
+M03=int(input("Cuantos has sacado M03:"))
+M05=int(input("Cuantos has sacado M05:"))
 if((estudiar=="mucho")and(M01==9)and(M02==10)and(M03==8)and((M05==6)and(M05<=8))):
 	print ("Puedes ganar 3000€ al mes")
 ponderada=(M01**0.3)+(M02**0.4)+(M03**0.25)+(M05**0.05)
