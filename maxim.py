56f4e2c31befbf00e377295c9d82f38ed72e0eb5
Ishamu Joseph <35691481+jishamu@users.noreply.github.com>
2018-11-15 10:31:40 +0100

    Create maxim.py

---
+++ maxim.py
@@ -0,0 +1,13 @@
+#llegir dos nombres i mostrarr el màxim
+## JO0C DE PROVES
+## NUMERO1              NUMERO2            SORTIDA
+     1                     1               igual
+    -1                     0               numero 2 es gran que numero 1
+     2                     1               numero 1 es gran que numero 2
+     
+numero1=int(input())
+numero1=int(input())
+if (numero1>numero2):
+   print(numero1, "es gran que", numero2)
+else:
+    print(numero2, "es gran que", numero1)
