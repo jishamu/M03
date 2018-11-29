a9f2f50505c42aaed57e1f76aa03f230b0e8a680
Ishamu Joseph <35691481+jishamu@users.noreply.github.com>
2018-11-29 22:16:10 +0100

    Update gasolinera.py

--- gasolinera/gasolinera.py
+++ gasolinera/gasolinera.py
@@ -1,1 +1,36 @@
-#coding ut8
+#coding:ut8
+galones = 3.785 #Galones por litro
+ 
+a = 50
+b = 55
+c = 60
+TotalA,TotalB,TotalC = 0,0,0
+ 
+tipo = input('Que tipo de gasolina desea [a,b,c] d = fin:')
+ 
+while tipo.upper() != 'D':
+    litros = int(input('Cuantos litros desea:'))
+    if tipo == 'a':
+        TipoA = litros * a
+        TotalA = TotalA + TipoA
+        print(TotalA)
+        gl = TipoA / galones
+    elif tipo == 'b':
+        TipoB = litros * b
+        TotalB = TotalB + TipoB
+        print(TotalB)
+        gl = TipoB / galones
+    elif tipo == 'c':
+        TipoC = litros * c
+        TotalC = TotalC + TipoC
+        print(TotalC)
+        gl = TipoC / galones
+    tipo = input('Que tipo de gasolina desea [a,b,c] d = fin:')
+ 
+print('\nTotal gasolina tipo A = %d Bs' %TotalA)
+print('Total gasolina tipo B = %d Bs' %TotalB)
+print('Total gasolina tipo C = %d Bs' %TotalC)
+ 
+total = TotalA + TotalB + TotalC
+ 
+print('Total recaudado = %d' %total)
